ed8f9e6e4d89f0eadb0e71526e5a6bd95de039fb
JWillemse <141319408+JacquesWillemse@users.noreply.github.com>
2026-05-09 12:58:35 +0200

    Tarot Card Scriptable Object

---
+++ Assets/Scripts/TarotCards.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Tarot Database", menuName = "Tarot Cards/Tarot Database")]
+public class TarotCards : ScriptableObject
+{
+    public List<TarotCardData> cards;
+}
+[System.Serializable]
+public class TarotCardData
+{
+    public string cardName;
+    public string description;
+    public Image tarotCardImage;
+}
